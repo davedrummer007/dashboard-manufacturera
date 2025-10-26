749a51d1711858ad3a58dbec66ea052153b979f4
davedrummer007 <davedrummer007@gmail.com>
2025-10-26 11:45:51 -0400

    feat: actualización completa del proyecto

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        navy: {
+          900: '#0F172A',
+          800: '#1E293B',
+          700: '#1E40AF'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
